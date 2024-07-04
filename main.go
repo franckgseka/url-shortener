ad6503a0ce64ee07b96a2a9ff12e84b5c18f5678
franckgseka <franckgseka@gmail.com>
2024-07-04 09:26:52 +0200

    first comm

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "url-shortener/database"
+    "url-shortener/handlers"
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    r := gin.Default()
+
+    // Initialize the database
+    database.Init()
+
+    r.POST("/shorten", handlers.CreateShortURL)
+    r.GET("/:shortURL", handlers.ResolveShortURL)
+
+    r.Run(":8080")
+}
